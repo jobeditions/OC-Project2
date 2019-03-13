20c103fc9ee039513bf25668001f3cadf82d9825
Ashok DEB <jobeditions@gmail.com>
2019-03-13 01:29:10 +0100

    Hobbies Section updated

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 
-
 $('.selected li').on('click',function(){
 	$(this).toggleClass('new');
 });
+
+$('.selector').on('click',function(){
+	$(this).toggleClass('new-hobbies');
+	$('footer').addClass('new-footer');
+	//$( ".new-footer" ).fadeOut( "slow", function() {
+    // Animation complete.
+  //});
+});
+
 
