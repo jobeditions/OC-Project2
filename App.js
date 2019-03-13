fc902952a31af94fe7b18c080a9b404b303c847f
Ashok DEB <jobeditions@gmail.com>
2019-03-13 11:49:53 +0100

    Bugs Removed Hobbies Section - Jquery

--- App.js
+++ App.js
@@ -3,12 +3,25 @@ $('.selected li').on('click',function(){
 	$(this).toggleClass('new');
 });
 
-$('.selector').on('click',function(){
-	$(this).toggleClass('new-hobbies');
-	$('footer').addClass('new-footer');
-	//$( ".new-footer" ).fadeOut( "slow", function() {
-    // Animation complete.
-  //});
-});
+
+$('.selector').on('click', function () {
+       
+        if ($(this).hasClass('new-hobbies')){
+            $(this).removeClass('new-hobbies');
+            $(this).addClass('reverse-hobbies');
+        } else {
+            $(this).addClass('new-hobbies');
+        }
+    });
+
+//$('.selector').on('click',function(){
+//$(this).toggleClass('new-hobbies');
+	//$('footer').addClass('new-footer');
+	//if($(this).hasClass('new-hobbies')){
+	//$(this).addClass('reverse-footer')	
+	
+	//}
+	
+//});
 
 
