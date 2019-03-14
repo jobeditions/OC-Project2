8480d0e96c41ecd3af423c24fe135a1d684f77ca
Ashok DEB <jobeditions@gmail.com>
2019-03-14 12:17:13 +0100

    Animation Self Section - Jquery

--- App.js
+++ App.js
@@ -25,3 +25,35 @@ $('.selector').on('click', function () {
 $('.photo').on('click', function () {
 $(this).toggleClass('nodisplay-photo');
 });
+
+function typeEffect(element, speed) {
+	var text = $(element).text();
+	$(element).html('');
+	
+	var i = 0;
+	var timer = setInterval(function() {
+					if (i < text.length) {
+						$(element).append(text.charAt(i));
+						i++;
+					} else {
+						clearInterval(timer);
+					}
+				}, speed);
+}
+
+
+$( document ).ready(function() {
+  var speed = 75;
+  var delay = $('h1.name').text().length * speed + speed;
+  
+  typeEffect($('h1.name'), speed);
+  setTimeout(function(){
+    $('h2.name-1').css('display','inline-block');
+    typeEffect($('h2.name-1'), speed);
+  }, delay);
+  setTimeout(function(){
+    $('p.name-2').css('display', 'inline-block');
+    typeEffect($('p.name-2'), speed);
+  }, delay);
+});
+
