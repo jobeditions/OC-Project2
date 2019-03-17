769a9e8350ead43a518455498c86939e398249de
Ashok DEB <jobeditions@gmail.com>
2019-03-17 23:59:08 +0100

    Building Up - MinWidth 400px

--- assets/js/App.js
+++ assets/js/App.js
@@ -93,7 +93,7 @@ $('.selector').on('click', function () {
   window.addEventListener('load',function () {
       var content = '';
       $("header").prepend('<div class="profile"></div>');
-      for (var i = 0; i < 270; i++) {
+      for (var i = 0; i < 342; i++) {
        content += "<span></span>" ;
        }
       $(".profile").wrapInner(content);
